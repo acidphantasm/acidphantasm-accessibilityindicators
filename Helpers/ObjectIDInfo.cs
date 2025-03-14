2a0661a82f25fcdcaf98f2689c017419039b25e0
acidphantasm <acidphantasm@acidphantasm.com>
2025-03-14 05:17:20 -0500

    Updating build stuff

--- Helpers/ObjectIDInfo.cs
+++ Helpers/ObjectIDInfo.cs
@@ -5,6 +5,5 @@ namespace acidphantasm_accessibilityindicators.Helpers
     internal class ObjectIDInfo: MonoBehaviour
     {
         public string _OwnerID = "none";
-        public Coroutine _Coroutine;
     }
 }
